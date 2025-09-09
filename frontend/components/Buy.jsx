919c290a3422a708f4775aca10fc63885ed84545
Bijoy <bijoy.ahmed12555@gmail.com>
2025-09-10 03:04:52 +0600

    Update: improve ui for better visibility

---
+++ frontend/components/Buy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Buy.css';
+
+export default function Buy() {
+  return (
+    <div className="buy-page">
+      <h1>Checkout</h1>
+      <p>Complete your purchase here.</p>
+      {/* Buy/checkout functionality will be implemented here */}
+    </div>
+  );
+}
