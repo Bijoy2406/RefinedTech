5978be95ea81811aa11f052d2083429ceb1f4eda
Bijoy <bijoy.ahmed12555@gmail.com>
2025-09-21 05:47:58 +0600

    Update composer.lock file

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -6,9 +6,10 @@ if [ -z "$APP_KEY" ]; then
   exit 1
 fi
 
-php artisan storage:link || true
+# These commands run at container start
 php artisan config:cache
 php artisan route:cache
+php artisan storage:link || true
 php artisan migrate --force --seed || true
 
 exec php -d variables_order=EGPCS -S 0.0.0.0:${PORT:-8080} public/index.php
